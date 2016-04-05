67c72ade940e5a0e695685022064d3455dbf0391
Nicolas Gama <nicolas.gama@uvsq.fr>
2016-04-05 21:11:32 +0000

    remove unnecessary functions from lweparams

--- lweparams.cpp
+++ lweparams.cpp
@@ -3,16 +3,6 @@
 
 using namespace std;
 
-//allocates and initialize lweParams
-LWEParams* new_lweParams(int n, double alpha) {
-	return new LWEParams(n,alpha);
-}
-
-//destructs and frees a lweParams structure
-void delete_lweParams(LWEParams* ptr) {
-	delete ptr;
-}
-
 LWEParams::LWEParams(int n, double alpha):
 		n(n),
 		alpha(alpha),
