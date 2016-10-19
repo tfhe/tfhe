72eaa07e2a3c9063719c64c4a58ae962f29ac0a6
Nicolas Gama <nicolas@inpher.io>
2016-10-19 21:19:32 +0200

    lwe unit test (except keyswitch)

---
+++ src/test/lwe_test.cpp
@@ -0,0 +1,277 @@
+#include <gtest/gtest.h>
+#include "../include/lwe-functions.h"
+#include "../include/numeric_functions.h"
+
+using namespace std;
+
+namespace {
+
+    class LweTest: public ::testing::Test {
+	protected:
+	    virtual void SetUp() {
+		params500 = new_LweParams(500,0.,1.);
+		params750 = new_LweParams(750,0.,1.);
+		params1024 = new_LweParams(1024,0.,1.);
+		key500 = new_LweKey(params500);
+		key750 = new_LweKey(params750);
+		key1024 = new_LweKey(params1024);
+	    }
+	    virtual void TearDown() {
+		delete_LweParams(params500);
+		delete_LweParams(params750);
+		delete_LweParams(params1024);
+		delete_LweKey(key500);
+		delete_LweKey(key750);
+		delete_LweKey(key1024);
+	    }
+
+	public:
+	    LweParams* params500;
+	    LweParams* params750;
+	    LweParams* params1024;
+	    LweKey* key500;
+	    LweKey* key750;
+	    LweKey* key1024;
+    };
+
+    // | frac(x) |
+    double absfrac(double d) {return abs(d-rint(d));}
+
+    // fills a LweSample with random Torus32
+    void fillRandom(LweSample* result, const LweParams* params) {
+	const int n = params->n;
+	for (int i=0; i<n; i++) result->a[i]=uniformTorus32_distrib(generator);
+	result->b=uniformTorus32_distrib(generator);
+	result->current_variance=0.2;
+    }
+
+    // copy a LweSample
+    void copySample(LweSample* result, const LweSample* sample, const LweParams* params) {
+	const int n = params->n;
+	for (int i=0; i<n; i++) result->a[i]=sample->a[i];
+	result->b=sample->b;
+	result->current_variance=sample->current_variance;
+    }
+
+
+    // This function generates a random Lwe key for the given parameters.
+    // The Lwe key for the result must be allocated and initialized
+    // (this means that the parameters are already in the result)
+    TEST_F(LweTest, lweKeyGen) {
+	vector<LweParams*> all_params = {params500, params750, params1024};
+	for (LweParams* params: all_params) {
+	    LweKey* key = new_LweKey(params);
+	    lweKeyGen(key);
+	    ASSERT_EQ(params,key->params);
+	    int n = key->params->n;
+	    int* s = key->key;
+	    //verify that the key is binary and kind-of random
+	    int count = 0;
+	    for (int i=0; i<n; i++) {
+		ASSERT_TRUE(s[i]==0 || s[i]==1);
+		count += s[i];
+	    }
+	    ASSERT_LE(count,n-20);
+	    ASSERT_GE(count,20);
+	    delete_LweKey(key);
+	}
+    }
+
+    // This function encrypts message by using key, with stdev alpha
+    // The Lwe sample for the result must be allocated and initialized
+    // (this means that the parameters are already in the result)
+    TEST_F (LweTest,lweSymEncryptPhaseDecrypt) {
+	static const int NB_SAMPLES=10;
+	static const int M = 8;
+	static const double alpha = 1./(10.*M);
+	vector<LweKey*> all_keys = {key500, key750, key1024};
+	for (LweKey* key: all_keys) {
+	    const LweParams* params = key->params;
+	    LweSample* samples = new_LweSample_array(NB_SAMPLES,params);
+	    //verify correctness of the decryption
+	    for (int trial=0; trial<NB_SAMPLES; trial++) {
+		Torus32 message = modSwitchToTorus32(trial,M);
+		lweSymEncrypt(&samples[trial],message,alpha,key);
+		Torus32 phase = lwePhase(&samples[trial],key);
+		Torus32 decrypt = lweSymDecrypt(&samples[trial],key,M);
+		double dmessage = t32tod(message);
+		double dphase = t32tod(phase);
+		ASSERT_EQ(message,decrypt);
+		ASSERT_LE(absfrac(dmessage-dphase),10.*alpha);
+		ASSERT_EQ(alpha*alpha,samples[trial].current_variance);
+	    }
+	    //verify that samples are random enough (all coordinates different)
+	    const int n = params->n;
+	    for (int i=0; i<n; i++) {
+		set<Torus32> testset;
+		for (int trial=0; trial<NB_SAMPLES; trial++) {
+		    testset.insert(samples[trial].a[i]);
+		}
+		ASSERT_GE(testset.size(),0.9*NB_SAMPLES);
+	    }
+	    delete_LweSample_array(NB_SAMPLES,samples);
+	}
+    }
+
+    //Arithmetic operations on Lwe samples
+    // result = (0,0)
+    TEST_F(LweTest, lweClear) {
+	vector<LweKey*> all_keys = {key500, key750, key1024};
+	for (LweKey* key: all_keys) {
+	    const LweParams* params = key->params;
+	    const int n = params->n;
+	    LweSample* sample = new_LweSample(params);
+	    fillRandom(sample,params);
+	    lweClear(sample,params);
+	    for (int i=0; i<n; i++) {
+		ASSERT_EQ(0,sample->a[i]);
+	    }
+	    ASSERT_EQ(0,sample->b);
+	    ASSERT_EQ(0.,sample->current_variance);
+	    delete_LweSample(sample);
+	}
+    }
+
+    // result = (0,mu)
+    TEST_F(LweTest, lweNoiselessTrivial) {
+	vector<LweKey*> all_keys = {key500, key750, key1024};
+	for (LweKey* key: all_keys) {
+	    const Torus32 message = uniformTorus32_distrib(generator);
+	    const LweParams* params = key->params;
+	    const int n = params->n;
+	    LweSample* sample = new_LweSample(params);
+	    fillRandom(sample,params);
+	    lweNoiselessTrivial(sample,message,params);
+	    for (int i=0; i<n; i++) {
+		ASSERT_EQ(0,sample->a[i]);
+	    }
+	    ASSERT_EQ(message,sample->b);
+	    ASSERT_EQ(0.,sample->current_variance);
+	    delete_LweSample(sample);
+	}
+    }
+
+
+    // result = result + sample */
+    //EXPORT void lweAddTo(LweSample* result, const LweSample* sample, const LweParams* params);
+    TEST_F(LweTest, lweAddTo) {
+	vector<LweKey*> all_keys = {key500, key750, key1024};
+	for (LweKey* key: all_keys) {
+	    const LweParams* params = key->params;
+	    const int n = params->n;
+	    LweSample* a = new_LweSample(params);
+	    LweSample* b = new_LweSample(params);
+	    LweSample* acopy = new_LweSample(params);
+	    fillRandom(a,params);
+	    fillRandom(b,params);
+	    copySample(acopy,a,params);
+	    lweAddTo(a,b,params);
+	    for (int i=0; i<n; i++) {
+		ASSERT_EQ(acopy->a[i] + b->a[i],a->a[i]);
+	    }
+	    ASSERT_EQ(acopy->b + b->b,a->b);
+	    ASSERT_EQ(acopy->current_variance + b->current_variance, a->current_variance);
+	    delete_LweSample(a);
+	    delete_LweSample(b);
+	    delete_LweSample(acopy);
+	}
+    }
+
+    // result = result - sample
+    //EXPORT void lweSubTo(LweSample* result, const LweSample* sample, const LweParams* params);
+    TEST_F(LweTest, lweSubTo) {
+	vector<LweKey*> all_keys = {key500, key750, key1024};
+	for (LweKey* key: all_keys) {
+	    const LweParams* params = key->params;
+	    const int n = params->n;
+	    LweSample* a = new_LweSample(params);
+	    LweSample* b = new_LweSample(params);
+	    LweSample* acopy = new_LweSample(params);
+	    fillRandom(a,params);
+	    fillRandom(b,params);
+	    copySample(acopy,a,params);
+	    lweSubTo(a,b,params);
+	    for (int i=0; i<n; i++) {
+		ASSERT_EQ(acopy->a[i] - b->a[i],a->a[i]);
+	    }
+	    ASSERT_EQ(acopy->b - b->b,a->b);
+	    ASSERT_EQ(acopy->current_variance + b->current_variance, a->current_variance);
+	    delete_LweSample(a);
+	    delete_LweSample(b);
+	    delete_LweSample(acopy);
+	}
+    }
+
+
+    // result = result + p.sample
+    //EXPORT void lweAddMulTo(LweSample* result, int p, const LweSample* sample, const LweParams* params);
+    TEST_F(LweTest, lweAddMulTo) {
+	const int p = 3;
+	vector<LweKey*> all_keys = {key500, key750, key1024};
+	for (LweKey* key: all_keys) {
+	    const LweParams* params = key->params;
+	    const int n = params->n;
+	    LweSample* a = new_LweSample(params);
+	    LweSample* b = new_LweSample(params);
+	    LweSample* acopy = new_LweSample(params);
+	    fillRandom(a,params);
+	    fillRandom(b,params);
+	    copySample(acopy,a,params);
+	    lweAddMulTo(a,p,b,params);
+	    for (int i=0; i<n; i++) {
+		ASSERT_EQ(acopy->a[i] + p * b->a[i],a->a[i]);
+	    }
+	    ASSERT_EQ(acopy->b + p * b->b,a->b);
+	    ASSERT_EQ(acopy->current_variance + p * p * b->current_variance, a->current_variance);
+	    delete_LweSample(a);
+	    delete_LweSample(b);
+	    delete_LweSample(acopy);
+	}
+    }
+
+    // result = result - p.sample
+    //EXPORT void lweSubMulTo(LweSample* result, int p, const LweSample* sample, const LweParams* params);
+    TEST_F(LweTest, lweSubMulTo) {
+	const int p = 3;
+	vector<LweKey*> all_keys = {key500, key750, key1024};
+	for (LweKey* key: all_keys) {
+	    const LweParams* params = key->params;
+	    const int n = params->n;
+	    LweSample* a = new_LweSample(params);
+	    LweSample* b = new_LweSample(params);
+	    LweSample* acopy = new_LweSample(params);
+	    fillRandom(a,params);
+	    fillRandom(b,params);
+	    copySample(acopy,a,params);
+	    lweSubMulTo(a,p,b,params);
+	    for (int i=0; i<n; i++) {
+		ASSERT_EQ(acopy->a[i] - p * b->a[i],a->a[i]);
+	    }
+	    ASSERT_EQ(acopy->b - p * b->b,a->b);
+	    ASSERT_EQ(acopy->current_variance + p * p * b->current_variance, a->current_variance);
+	    delete_LweSample(a);
+	    delete_LweSample(b);
+	    delete_LweSample(acopy);
+	}
+    }
+
+#if 0
+
+    //TODO: à tester!!
+    
+    // 
+    // creates a Key Switching Key between the two keys
+    ///
+    EXPORT void lweCreateKeySwitchKey(LweKeySwitchKey* result, const LweKey* in_key, const LweKey* out_key);
+
+
+    //
+    // applies keySwitching
+    ///
+    EXPORT void lweKeySwitch(LweSample* result, const LweKeySwitchKey* ks, const LweSample* sample);
+
+#endif //Lwe_FUNCTIONS_H
+
+
+
+}//namespace
