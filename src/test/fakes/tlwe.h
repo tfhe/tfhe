e3f935ebf512110cb58745f4fa623d5d02c04d1f
Ilaria Chillotti <ilaria.chillotti@uvsq.fr>
2017-02-20 17:34:58 +0100

    small modifs

---
+++ src/test/fakes/tlwe.h
@@ -0,0 +1,38 @@
+/* **************************
+Fakes for TLWE 
+************************** */
+
+
+#ifndef FAKE_TLWE_H
+#define FAKE_TLWE_H
+
+
+#include "tfhe.h"
+
+
+// Fake symetric encryption of a Torus message
+inline void fake_tLweSymEncryptT(TLweSample* result, Torus32 message, double alpha, const TLweKey* key) {
+    tLweNoiselessTrivialT(result,message,key->params);
+    result->current_variance=alpha*alpha;
+}
+
+// Fake symetric encryption of a Torus polynomial message
+inline void fake_tLweSymEncrypt(TLweSample* result, TorusPolynomial* mu, double alpha, const TLweKey* key) {
+    tLweNoiselessTrivial(result,message,key->params);
+    result->current_variance=alpha*alpha;
+}
+
+
+
+inline Torus32 fake_tLweMessage(const TLweSample* sample) {
+    return sample->b;
+}
+
+inline Torus32 fake_tLweVariance(const TLweSample* sample) {
+    return sample->current_variance;
+}
+
+
+
+
+#endif //FAKE_TLWE_H
