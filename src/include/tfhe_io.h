96aa14490892f8484bd6e3ee5ee11c711a53d935
Nicolas Gama <nicolas@inpher.io>
2017-03-16 20:37:03 +0100

    refactor and comment generic streams

--- src/include/tfhe_io.h
+++ src/include/tfhe_io.h
@@ -42,21 +42,75 @@ EXPORT LweParams* new_lweParams_fromStream(std::istream& in);
 
 //---------------------------------------------------------------
 
+/**
+ * This is a generic Istream wrapper: supports getLine() and feof()
+ */
+class Istream {
+    public:
+	/**
+	 * this function reads a line from the stream F, and stores the result in the string reps
+	 * '\r' characters are ignored, 
+	 * '\n' stops the reading, and is not included in the result
+	 */
+	virtual void getLine(std::string& reps) const=0;
+	/**
+	 * tests for the end of the inputstream
+	 */
+	virtual bool feof() const=0;
+	virtual ~Istream() {};
+};
+
+/**
+ * This is a generic Ostream wrapper: supports fputs()
+ */
+class Ostream {
+    public:
+	/**
+	 * prints a string in the outputstream. No endline character is added.
+	 */
+	virtual void fputs(const std::string& s) const=0;
+	virtual ~Ostream() {};
+};
+
+/**
+ * This is a property map: a title and a set of name-value properties.
+ * For convenience, this class accepts string, long or doubles for 
+ * the property values, with implicit conversion between them.
+ */
 class TextModeProperties {
     public:
 	virtual const std::string& getTypeTitle() const =0;
-	virtual const std::string& getProperty(const std::string& name) const =0;
 	virtual void setTypeTitle(const std::string& title)=0;
+
+	virtual const std::string& getProperty(const std::string& name) const =0;
+	virtual double getProperty_double(const std::string& name) const=0;
+	virtual long getProperty_long(const std::string& name) const=0;
 	virtual void setProperty(const std::string& name, const std::string& value)=0;
+	virtual void setProperty_double(const std::string& name, double value)=0;
+	virtual void setProperty_long(const std::string& name, long value)=0;
 	virtual ~TextModeProperties() {}
 };
 
-TextModeProperties* new_TextModeProperties_fromFile(FILE* F);
-TextModeProperties* new_TextModeProperties_fromStream(std::istream& F);
-void delete_TextModeProperties(TextModeProperties* ptr);
-void print_TextModeProperties_toFile(FILE* F, const TextModeProperties* props);
-void print_TextModeProperties_toStream(std::ostream& F, const TextModeProperties* props);
+/**
+ * This constructor function reads a property map from a generic stream.
+ */
+TextModeProperties* new_TextModeProperties_fromIstream(const Istream& in);
 
+/**
+ * This constructor function creates a blank property map.
+ */
+TextModeProperties* new_TextModeProperties_blank();
+
+/**
+ * This destructor must be called to release a TextModeProperties
+ * instance.
+ */
+void delete_TextModeProperties(TextModeProperties* ptr);
+
+/**
+ * Prints a property map to a generic Ostream.
+ */
+void print_TextModeProperties_toOStream(const Ostream& out, const TextModeProperties* props);
 
 #endif
 
--- src/libtfhe/tfhe_io.cpp
+++ src/libtfhe/tfhe_io.cpp
@@ -4,47 +4,109 @@
 
 using namespace std;
 
+/**
+ * implementation of a property map (backed by a map of strings)
+ */
 class MapTextModeProperties: public TextModeProperties {
     map<string, string> data;
     string title;
     public:
         MapTextModeProperties() {}
-	virtual const std::string& getTypeTitle() const {return title; }
-	virtual const std::string& getProperty(const std::string& name) const { return data.at(name); }
-	virtual void setTypeTitle(const std::string& title) { this->title=title; }
-	virtual void setProperty(const std::string& name, const std::string& value) { data[name]=value; }
-	const map<string, string>& getMap() const { return data; }
+	virtual const std::string& getTypeTitle() const {
+	    return title; 
+	}
+	virtual const std::string& getProperty(const std::string& name) const {
+	    return data.at(name); 
+	}
+	virtual void setTypeTitle(const std::string& title) {
+	    this->title=title; 
+	}
+	virtual void setProperty(const std::string& name, const std::string& value) {
+	    data[name]=value; 
+	}
+	virtual double getProperty_double(const std::string& name) const { 
+	    return stold(getProperty(name)); 
+	}
+	virtual long getProperty_long(const std::string& name) const { 
+	    return stol(getProperty(name)); 
+	}
+	virtual void setProperty_double(const std::string& name, double value) {
+	    char buf[64]; sprintf(buf,"%.8lf",value); setProperty(name,buf); 
+	}
+	virtual void setProperty_long(const std::string& name, long value) {
+	    char buf[64]; sprintf(buf,"%ld",value); setProperty(name,buf); 
+	}
+	const map<string, string>& getMap() const {
+	    return data; 
+	}
 	virtual ~MapTextModeProperties() {}
 };
 
 
-void getline(FILE* F, string& reps) {
-    reps.clear();
-    for (int c = fgetc(F); c!=EOF; c = fgetc(F)) {
-	if (c=='\r') continue;
-	if (c=='\n') return;
-	reps.push_back(c);
+
+class StdIstream : public Istream {
+    istream& in;
+    public:
+    StdIstream(istream& in): in(in) {}
+    virtual void getLine(string& reps) const { std::getline(in, reps); }
+    virtual bool feof() const { return ! ((bool) in); }
+    virtual ~StdIstream() {};
+};
+
+class CIstream : public Istream {
+    FILE* F;
+    public:
+    CIstream(FILE* F): F(F) {}
+    virtual void getLine(string& reps) const { 
+	reps.clear();
+	for (int c = fgetc(F); c!=EOF; c = fgetc(F)) {
+	    if (c=='\r') continue;
+	    if (c=='\n') return;
+	    reps.push_back(c);
+	}
     }
-}
-
-bool feof(istream& in) { return ! ((bool) in); }
-
-void fputs(ostream& out, const string& line) {
-    out << line;
-}
-
-void fputs(FILE* F, const string& line) {
-    fputs(F,line.c_str());
-}
+    virtual bool feof() const { return std::feof(F); }
+    virtual ~CIstream() {};
+};
 
 
-template<typename STREAM>
-TextModeProperties* new_TextModeProperties(STREAM& F) {
+class StdOstream : public Ostream{
+    ostream& out;
+    public:
+    StdOstream(ostream& out): out(out) {}
+    virtual void fputs(const string& s) const { out << s; }
+    virtual ~StdOstream() {};
+};
+
+class COstream : public Ostream {
+    FILE* F;
+    public:
+    COstream(FILE* F): F(F) {}
+    virtual void fputs(const string& s) const { 
+	std::fputs(s.c_str(), F);
+    }
+    virtual ~COstream() {};
+};
+
+
+/**
+ * This function parses a file, and returns its content as a
+ * new TextModeProperty instance.
+ * The instance must be subsequently destroyed with delete_TextModeProperties
+ *
+ * The format of the serialized data looks like:
+ * -----BEGIN LWEPARAMS-----
+ * alpha_max: 0.30000000
+ * alpha_min: 0.10000000
+ * n: 500
+ * -----END LWEPARAMS-----  
+ */
+TextModeProperties* new_TextModeProperties_fromIstream(const Istream& F) {
     MapTextModeProperties* reps = new MapTextModeProperties();
     string line;
     string endDelimitor;
     bool content_started=false;
-    for (getline(F, line); !feof(F); getline(F, line)) {
+    for (F.getLine(line); !F.feof(); F.getLine(line)) {
 	int n = line.length();
 	if (!line.compare(0,11,"-----BEGIN ") && !line.compare(n-5,5,"-----")) {
 	    string titleType = line.substr(11, n-16);
@@ -70,42 +132,57 @@ TextModeProperties* new_TextModeProperties(STREAM& F) {
     return NULL;
 }
 
-template<typename STREAM>
-void print_TextModeProperties(STREAM& F, const TextModeProperties* properties) {
+/**
+ * This serializes a TextModeProperty instance.
+ *
+ * The format of the serialized data looks like:
+ * -----BEGIN LWEPARAMS-----
+ * alpha_max: 0.30000000
+ * alpha_min: 0.10000000
+ * n: 500
+ * -----END LWEPARAMS-----  
+ */
+void print_TextModeProperties_toOStream(const Ostream& F, const TextModeProperties* properties) {
     const MapTextModeProperties* props = (const MapTextModeProperties*) properties;
-    fputs(F, string("-----BEGIN ")+props->getTypeTitle()+string("-----\n"));
+    F.fputs(string("-----BEGIN ")+props->getTypeTitle()+string("-----\n"));
     for (auto it: props->getMap()) {
-	fputs(F, it.first + string(": ") + it.second+"\n");
+	F.fputs(it.first + string(": ") + it.second+"\n");
     }
-    fputs(F, string("-----END ")+props->getTypeTitle()+string("-----\n"));
+    F.fputs(string("-----END ")+props->getTypeTitle()+string("-----\n"));
 }
 
-TextModeProperties* new_TextModeProperties_fromFile(FILE* F) { return new_TextModeProperties(F); }
-TextModeProperties* new_TextModeProperties_fromStream(std::istream& F) { return new_TextModeProperties(F); }
-TextModeProperties* new_TextModeProperties_blank() { return new MapTextModeProperties(); }
-void delete_TextModeProperties(TextModeProperties* ptr) { delete ptr; }
-void print_TextModeProperties_toFile(FILE* F, const TextModeProperties* props) { print_TextModeProperties(F, props); }
-void print_TextModeProperties_toStream(ostream& F, const TextModeProperties* props) { print_TextModeProperties(F, props); }
+TextModeProperties* new_TextModeProperties_blank() { 
+    return new MapTextModeProperties(); 
+}
+
+void delete_TextModeProperties(TextModeProperties* ptr) { 
+    delete ptr; 
+}
 
 
-template<typename STREAM>
-void export_lweParams(STREAM& F, const LweParams* lweparams) {
+/**
+ * This function prints the lwe parameters to a generic stream
+ */
+void export_lweParams(const Ostream& F, const LweParams* lweparams) {
     TextModeProperties* props = new_TextModeProperties_blank();
     props->setTypeTitle("LWEPARAMS");
-    props->setProperty("n", to_string(lweparams->n));
-    props->setProperty("alpha_min", to_string(lweparams->alpha_min));
-    props->setProperty("alpha_max", to_string(lweparams->alpha_max));
-    print_TextModeProperties(F, props);
+    props->setProperty_long("n", lweparams->n);
+    props->setProperty_double("alpha_min", lweparams->alpha_min);
+    props->setProperty_double("alpha_max", lweparams->alpha_max);
+    print_TextModeProperties_toOStream(F, props);
     delete_TextModeProperties(props);
 }
 
-template<typename STREAM>
-LweParams* read_new_lweParams(STREAM& F) {
-    TextModeProperties* props = new_TextModeProperties(F);
+/**
+ * This constructor function reads and creates a LWEParams from a generic stream. The result
+ * must be deleted with delete_lweParams();
+ */
+LweParams* read_new_lweParams(const Istream& F) {
+    TextModeProperties* props = new_TextModeProperties_fromIstream(F);
     if (props->getTypeTitle() != string("LWEPARAMS")) abort();
-    int n = stoi(props->getProperty("n"));
-    double alpha_min = stold(props->getProperty("alpha_min"));
-    double alpha_max = stold(props->getProperty("alpha_max"));
+    int n = props->getProperty_long("n");
+    double alpha_min = props->getProperty_double("alpha_min");
+    double alpha_max = props->getProperty_double("alpha_max");
     delete_TextModeProperties(props);
     return new_LweParams(n,alpha_min,alpha_max);
 }
@@ -114,23 +191,23 @@ LweParams* read_new_lweParams(STREAM& F) {
 /**
  * This function prints the lwe parameters to a file
  */
-EXPORT void export_lweParams_toFile(FILE* F, const LweParams* lweparams) { export_lweParams(F,lweparams); }
+EXPORT void export_lweParams_toFile(FILE* F, const LweParams* lweparams) { export_lweParams(COstream(F),lweparams); }
 
 /**
  * This constructor function reads and creates a LWEParams from a File. The result
  * must be deleted with delete_lweParams();
  */
-EXPORT void export_lweParams_toStream(ostream& F, const LweParams* lweparams) { export_lweParams(F,lweparams); }
+EXPORT void export_lweParams_toStream(ostream& F, const LweParams* lweparams) { export_lweParams(StdOstream(F),lweparams); }
 
 /**
  * This constructor function reads and creates a LWEParams from a stream. The result
  * must be deleted with delete_lweParams();
  */
-EXPORT LweParams* new_lweParams_fromStream(std::istream& in) { return read_new_lweParams(in); }
+EXPORT LweParams* new_lweParams_fromStream(std::istream& in) { return read_new_lweParams(StdIstream(in)); }
 
 /**
  * This constructor function reads and creates a LWEParams from a File. The result
  * must be deleted with delete_lweParams();
  */
-EXPORT LweParams* new_lweParams_fromFile(FILE* F)  { return read_new_lweParams(F); }
+EXPORT LweParams* new_lweParams_fromFile(FILE* F)  { return read_new_lweParams(CIstream(F)); }
 
