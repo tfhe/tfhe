8c0a3b76342fa6025141021a2e28d0beef5b6b61
Ilaria Chillotti <ilaria.chillotti@uvsq.fr>
2017-03-21 17:48:01 +0100

    export params for TLWE

--- src/include/tfhe_io.h
+++ src/include/tfhe_io.h
@@ -5,6 +5,8 @@
 #include "lwekey.h"
 #include "lweparams.h"
 #include "lwesamples.h"
+#include "tlwe.h"
+#include "tgsw.h"
 
 #ifdef __cplusplus
 #include <cstdio>
@@ -13,6 +15,11 @@
 #include <stdio.h>
 #endif
 
+
+
+/* ****************************
+ * LWE params
+**************************** */
 
 /**
  * This function prints the lwe parameters to a file
@@ -40,6 +47,94 @@ EXPORT void export_lweParams_toStream(std::ostream& out, const LweParams* lwepar
 EXPORT LweParams* new_lweParams_fromStream(std::istream& in);
 
 #endif
+
+
+
+
+
+
+
+
+
+
+/* ****************************
+ * TLWE params
+**************************** */
+
+/**
+ * This function prints the tLwe parameters to a file
+ */
+EXPORT void export_tLweParams_toFile(FILE* F, const TLweParams* tlweparams);
+
+/**
+ * This constructor function reads and creates a TLWEParams from a File. The result
+ * must be deleted with delete_TLweParams();
+ */
+EXPORT TLweParams* new_tLweParams_fromFile(FILE* F);
+
+#ifdef __cplusplus
+
+/**
+ * This constructor function reads and creates a TLWEParams from a File. The result
+ * must be deleted with delete_TLweParams();
+ */
+EXPORT void export_tLweParams_toStream(std::ostream& F, const TLweParams* tlweparams);
+
+/**
+ * This constructor function reads and creates a TLWEParams from a stream. The result
+ * must be deleted with delete_TLweParams();
+ */
+EXPORT TLweParams* new_tLweParams_fromStream(std::istream& in);
+
+#endif
+
+
+
+
+
+
+
+
+
+
+/* ****************************
+ * TGSW params
+**************************** */
+
+/**
+ * This function prints the tLwe parameters to a file
+ */
+EXPORT void export_tGswParams_toFile(FILE* F, const TGswParams* tgswparams);
+
+/**
+ * This constructor function reads and creates a TGSWParams from a File. The result
+ * must be deleted with delete_TGswParams();
+ */
+EXPORT TGswParams* new_tGswParams_fromFile(FILE* F);
+
+
+
+#ifdef __cplusplus
+
+/**
+ * This constructor function reads and creates a TGSWParams from a File. The result
+ * must be deleted with delete_TGswParams();
+ */
+EXPORT void export_tGswParams_toStream(std::ostream& F, const TGswParams* tgswparams);
+
+/**
+ * This constructor function reads and creates a TGSWParams from a stream. The result
+ * must be deleted with delete_TGswParams();
+ */
+EXPORT TGswParams* new_tGswParams_fromStream(std::istream& in);
+
+
+#endif
+
+
+
+
+
 
 #endif // TFHE_IO_H
 
--- src/libtfhe/tfhe_io.cpp
+++ src/libtfhe/tfhe_io.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+
+
+
+/* ****************************
+ * LWE params
+**************************** */
+
 /**
  * This function prints the lwe parameters to a generic stream
  */
@@ -56,3 +63,151 @@ EXPORT LweParams* new_lweParams_fromStream(std::istream& in) { return read_new_l
  */
 EXPORT LweParams* new_lweParams_fromFile(FILE* F)  { return read_new_lweParams(to_Istream(F)); }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ****************************
+ * TLWE params
+**************************** */
+
+/**
+ * This function prints the tlwe parameters to a generic stream
+ */
+void export_tLweParams(const Ostream& F, const TLweParams* tlweparams) {
+    TextModeProperties* props = new_TextModeProperties_blank();
+    props->setTypeTitle("TLWEPARAMS");
+    props->setProperty_long("N", tlweparams->N);
+    props->setProperty_long("k", tlweparams->k);
+    props->setProperty_double("alpha_min", tlweparams->alpha_min);
+    props->setProperty_double("alpha_max", tlweparams->alpha_max);
+    print_TextModeProperties_toOStream(F, props);
+    delete_TextModeProperties(props);
+}
+
+
+/**
+ * This constructor function reads and creates a TLweParams from a generic stream. The result
+ * must be deleted with delete_TLweParams();
+ */
+TLweParams* read_new_tLweParams(const Istream& F) {
+    TextModeProperties* props = new_TextModeProperties_fromIstream(F);
+    if (props->getTypeTitle() != string("TLWEPARAMS")) abort();
+    int N = props->getProperty_long("N");
+    int k = props->getProperty_long("k");
+    double alpha_min = props->getProperty_double("alpha_min");
+    double alpha_max = props->getProperty_double("alpha_max");
+    delete_TextModeProperties(props);
+    return new_TLweParams(N,k,alpha_min,alpha_max);
+}
+
+
+/**
+ * This function prints the tLwe parameters to a file
+ */
+EXPORT void export_tLweParams_toFile(FILE* F, const TLweParams* tlweparams) { export_tLweParams(to_Ostream(F),tlweparams); }
+
+/**
+ * This constructor function reads and creates a TLWEParams from a File. The result
+ * must be deleted with delete_TLweParams();
+ */
+EXPORT void export_tLweParams_toStream(ostream& F, const TLweParams* tlweparams) { export_tLweParams(to_Ostream(F),tlweparams); }
+
+/**
+ * This constructor function reads and creates a TLWEParams from a stream. The result
+ * must be deleted with delete_TLweParams();
+ */
+EXPORT TLweParams* new_tLweParams_fromStream(std::istream& in) { return read_new_tLweParams(to_Istream(in)); }
+
+/**
+ * This constructor function reads and creates a TLWEParams from a File. The result
+ * must be deleted with delete_TLweParams();
+ */
+EXPORT TLweParams* new_tLweParams_fromFile(FILE* F)  { return read_new_tLweParams(to_Istream(F)); }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ****************************
+ * TGSW params
+**************************** */
+
+/**
+ * This function prints the tGsw parameters to a generic stream
+ */
+void export_tGswParams(const Ostream& F, const TGswParams* tgswparams) {
+    TextModeProperties* props = new_TextModeProperties_blank();
+    props->setTypeTitle("TGSWPARAMS");
+    props->setProperty_long("l", tgswparams->l);
+    props->setProperty_long("Bgbit", tgswparams->Bgbit);
+
+    // un set property pour const TLweParams* tlweparams
+
+
+
+
+
+
+    print_TextModeProperties_toOStream(F, props);
+    delete_TextModeProperties(props);
+}
+
+
+/**
+ * This constructor function reads and creates a TGswParams from a generic stream. The result
+ * must be deleted with delete_TGswParams();
+ */
+TGswParams* read_new_tGswParams(const Istream& F) {
+    TextModeProperties* props = new_TextModeProperties_fromIstream(F);
+    if (props->getTypeTitle() != string("TGSWPARAMS")) abort();
+    int l = props->getProperty_long("l");
+    int Bgbit = props->getProperty_long("Bgbit");
+    
+    // TLweParams* tlweparams = props->getProperty_...
+
+    delete_TextModeProperties(props);
+    return new_TGswParams(l,Bgbit,tlweparams);
+}
+
+
+/**
+ * This function prints the tLwe parameters to a file
+ */
+EXPORT void export_tGswParams_toFile(FILE* F, const TGswParams* tgswparams) { export_tGswParams(to_Ostream(F),tgswparams); }
+
+/**
+ * This constructor function reads and creates a TGSWParams from a File. The result
+ * must be deleted with delete_TGswParams();
+ */
+EXPORT void export_tGswParams_toStream(ostream& F, const TGswParams* tgswparams) { export_tGswParams(to_Ostream(F),tgswparams); }
+
+/**
+ * This constructor function reads and creates a TGSWParams from a stream. The result
+ * must be deleted with delete_TGswParams();
+ */
+EXPORT TGswParams* new_tGswParams_fromStream(std::istream& in) { return read_new_tGswParams(to_Istream(in)); }
+
+/**
+ * This constructor function reads and creates a TGSWParams from a File. The result
+ * must be deleted with delete_TGswParams();
+ */
+EXPORT TGswParams* new_tGswParams_fromFile(FILE* F)  { return read_new_tGswParams(to_Istream(F)); }
