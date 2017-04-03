1abd332dbf2e52ca886be59e5df94570496b9220
Ilaria Chillotti <ilaria.chillotti@uvsq.fr>
2017-04-03 18:42:21 +0200

    boot gates implemented

--- src/libtfhe/boot-gates.cpp
+++ src/libtfhe/boot-gates.cpp
@@ -27,8 +27,8 @@ using namespace std;
 
 /*
  * Homomorphic bootstrapped NAND gate
- * Takes in input 2 LWE samples (with message space [0,1/4]*vs[-1/8,1/8], noise<1/16)
- * Outputs a LWE bootstrapped sample (with message space [0,1/4]*vs[?,?], noise<1/16)
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
 */
 EXPORT void bootsNAND(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) {
 	static const Torus32 MU = modSwitchToTorus32(1,8);
@@ -50,31 +50,344 @@ EXPORT void bootsNAND(LweSample* result, const LweSample* ca, const LweSample* c
 }
 
 
-/** bootstrapped Or Gate:  */ 
-EXPORT void bootsOR(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped And Gate: result = a and b */ 
-EXPORT void bootsAND(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped Xor Gate: result = a xor b */ 
-EXPORT void bootsXOR(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped Xnor Gate: result = (a==b) */ 
-EXPORT void bootsXNOR(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped Not Gate: result = not(a) */ 
-EXPORT void bootsNOT(LweSample* result, const LweSample* ca, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped Copy Gate: result = a */ 
-EXPORT void bootsCOPY(LweSample* result, const LweSample* ca, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped Nor Gate: result = not(a or b) */ 
-EXPORT void bootsNOR(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped AndNY Gate: not(a) and b */ 
-EXPORT void bootsANDNY(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped AndYN Gate: a and not(b) */ 
-EXPORT void bootsANDYN(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped OrNY Gate: not(a) or b */ 
-EXPORT void bootsORNY(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
-/** bootstrapped OrYN Gate: a or not(b) */ 
-EXPORT void bootsORYN(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
+/*
+ * Homomorphic bootstrapped OR gate
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsOR(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
 
-/** bootstrapped Mux(a,b,c) = a?b:c */ 
-EXPORT void bootsMUX(LweSample* result, const LweSample* a, const LweSample* b, const LweSample* c, const TFheGateBootstrappingCloudKeySet* bk) { die_dramatically("Not implemented"); }
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,1/8) + ca + cb
+	static const Torus32 OrConst=modSwitchToTorus32(1,8);
+	lweNoiselessTrivial(temp_result, OrConst, in_out_params); 
+	lweAddTo(temp_result, ca, in_out_params);
+	lweAddTo(temp_result, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result);
+}
+
+
+/*
+ * Homomorphic bootstrapped AND gate
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsAND(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,-1/8) + ca + cb
+	static const Torus32 AndConst=modSwitchToTorus32(-1,8);
+	lweNoiselessTrivial(temp_result, AndConst, in_out_params); 
+	lweAddTo(temp_result, ca, in_out_params);
+	lweAddTo(temp_result, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result); 
+}
+
+
+/*
+ * Homomorphic bootstrapped XOR gate
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsXOR(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,1/4) + 2*(ca + cb)
+	static const Torus32 XorConst=modSwitchToTorus32(1,4);
+	lweNoiselessTrivial(temp_result, XorConst, in_out_params); 
+	lweAddMulTo(temp_result, 2, ca, in_out_params);
+	lweAddMulTo(temp_result, 2, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result); 
+}
+
+
+/*
+ * Homomorphic bootstrapped XNOR gate
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsXNOR(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,-1/4) + 2*(-ca-cb)
+	static const Torus32 XnorConst=modSwitchToTorus32(-1,4);
+	lweNoiselessTrivial(temp_result, XnorConst, in_out_params); 
+	lweSubMulTo(temp_result, 2, ca, in_out_params);
+	lweSubMulTo(temp_result, 2, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result); 
+}
+
+
+/*
+ * Homomorphic bootstrapped NOT gate (doesn't need to be bootstrapped)
+ * Takes in input 1 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsNOT(LweSample* result, const LweSample* ca, const TFheGateBootstrappingCloudKeySet* bk) { 
+	const LweParams* in_out_params = bk->params->in_out_params;
+	const int n = in_out_params->n;
+
+    //compute: -ca
+	for (int i = 0; i < n; ++i) result->a[i] = -ca->a[i];
+    result->b = -ca->b;
+    result->current_variance = ca->current_variance; 
+}
+
+
+/*
+ * Homomorphic bootstrapped COPY gate (doesn't need to be bootstrapped)
+ * Takes in input 1 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsCOPY(LweSample* result, const LweSample* ca, const TFheGateBootstrappingCloudKeySet* bk) { 
+	const LweParams* in_out_params = bk->params->in_out_params;
+	const int n = in_out_params->n;
+
+    //compute: result = ca
+	for (int i = 0; i < n; ++i) result->a[i] = ca->a[i];
+    result->b = ca->b;
+    result->current_variance = ca->current_variance;
+}
+
+
+/*
+ * Homomorphic bootstrapped NOR gate
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsNOR(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,-1/8) - ca - cb
+	static const Torus32 NorConst=modSwitchToTorus32(-1,8);
+	lweNoiselessTrivial(temp_result, NorConst, in_out_params); 
+	lweSubTo(temp_result, ca, in_out_params);
+	lweSubTo(temp_result, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result);
+}
+
+
+/*
+ * Homomorphic bootstrapped AndNY Gate: not(a) and b
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsANDNY(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,-1/8) - ca + cb
+	static const Torus32 AndNYConst=modSwitchToTorus32(-1,8);
+	lweNoiselessTrivial(temp_result, AndNYConst, in_out_params); 
+	lweSubTo(temp_result, ca, in_out_params);
+	lweAddTo(temp_result, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result); 
+}
+
+
+/*
+ * Homomorphic bootstrapped AndYN Gate: a and not(b)
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/
+EXPORT void bootsANDYN(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,-1/8) + ca - cb
+	static const Torus32 AndYNConst=modSwitchToTorus32(-1,8);
+	lweNoiselessTrivial(temp_result, AndYNConst, in_out_params); 
+	lweAddTo(temp_result, ca, in_out_params);
+	lweSubTo(temp_result, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result);
+}
+
+
+/*
+ * Homomorphic bootstrapped OrNY Gate: not(a) or b
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/ 
+EXPORT void bootsORNY(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,1/8) - ca + cb
+	static const Torus32 OrNYConst=modSwitchToTorus32(1,8);
+	lweNoiselessTrivial(temp_result, OrNYConst, in_out_params); 
+	lweSubTo(temp_result, ca, in_out_params);
+	lweAddTo(temp_result, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result); 
+}
+
+
+/*
+ * Homomorphic bootstrapped OrYN Gate: a or not(b)
+ * Takes in input 2 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/ 
+EXPORT void bootsORYN(LweSample* result, const LweSample* ca, const LweSample* cb, const TFheGateBootstrappingCloudKeySet* bk) { 
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result = new_LweSample(in_out_params); 
+
+	//compute: (0,1/8) + ca - cb
+	static const Torus32 OrYNConst=modSwitchToTorus32(1,8);
+	lweNoiselessTrivial(temp_result, OrYNConst, in_out_params); 
+	lweAddTo(temp_result, ca, in_out_params);
+	lweSubTo(temp_result, cb, in_out_params);
+
+	//if the phase is positive, the result is 1/8
+	//if the phase is positive, else the result is -1/8
+	tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
+
+	delete_LweSample(temp_result); 
+}
+
+
+
+
+/*
+ * Homomorphic bootstrapped Mux(a,b,c) = a?b:c = a*b + not(a)*c
+ * Takes in input 3 LWE samples (with message space [-1/8,1/8], noise<1/16)
+ * Outputs a LWE bootstrapped sample (with message space [-1/8,1/8], noise<1/16)
+*/ 
+EXPORT void bootsMUX(LweSample* result, const LweSample* a, const LweSample* b, const LweSample* c, const TFheGateBootstrappingCloudKeySet* bk) { 
+	const TGswParams* bk_params = bk->bkFFT->bk_params;
+    const TLweParams* accum_params = bk->bkFFT->accum_params;
+    const LweParams* extract_params = &accum_params->extracted_lweparams;
+    const LweParams* in_params = bk->bkFFT->in_out_params;
+    const int N=accum_params->N;
+    const int Nx2= 2*N;
+    const int n = in_params->n;
+
+	static const Torus32 MU = modSwitchToTorus32(1,8);
+	const LweParams* in_out_params = bk->params->in_out_params;
+
+	LweSample* temp_result1 = new_LweSample(in_out_params); 
+	LweSample* temp_result2 = new_LweSample(in_out_params); 
+
+
+	//compute "AND(a,b)": (0,-1/8) + a + b
+	static const Torus32 AndConst=modSwitchToTorus32(-1,8);
+	lweNoiselessTrivial(temp_result1, AndConst, in_out_params); 
+	lweAddTo(temp_result1, a, in_out_params);
+	lweAddTo(temp_result1, b, in_out_params);
+
+	TorusPolynomial* testvect1 = new_TorusPolynomial(N);
+    int* bara1 = new int[N];
+    LweSample* u1 = new_LweSample(extract_params);
+	// Modulus switching
+    int barb1 = modSwitchFromTorus32(temp_result1->b,Nx2);
+    for (int i=0; i<n; i++) {
+        bara1[i]=modSwitchFromTorus32(temp_result1->a[i],Nx2);
+    }
+    // the initial testvec = [mu,mu,mu,...,mu]
+    for (int i=0;i<N;i++) testvect1->coefsT[i]=MU;
+    // Bootstrapping rotation and extraction
+    tfhe_blindRotateAndExtract_FFT(u1, testvect1, bk->bkFFT->bkFFT, barb1, bara1, n, bk_params);
+    
+
+	//compute "AND(not(a),c)": (0,-1/8) - a + c
+	lweNoiselessTrivial(temp_result2, AndConst, in_out_params); 
+	lweSubTo(temp_result2, a, in_out_params);
+	lweAddTo(temp_result2, c, in_out_params);
+
+	TorusPolynomial* testvect2 = new_TorusPolynomial(N);
+    int* bara2 = new int[N];
+    LweSample* u2 = new_LweSample(extract_params);
+	// Modulus switching
+    int barb2 = modSwitchFromTorus32(temp_result2->b,Nx2);
+    for (int i=0; i<n; i++) {
+        bara2[i]=modSwitchFromTorus32(temp_result2->a[i],Nx2);
+    }
+    // the initial testvec = [mu,mu,mu,...,mu]
+    for (int i=0;i<N;i++) testvect2->coefsT[i]=MU;
+    // Bootstrapping rotation and extraction
+    tfhe_blindRotateAndExtract_FFT(u2, testvect2, bk->bkFFT->bkFFT, barb2, bara2, n, bk_params);
+
+
+	// Add u1=u1+u2
+	lweAddTo(u1, u2, in_out_params);
+	// Key switching
+    lweKeySwitch(result, bk->bkFFT->ks, u1);
+
+
+    delete_LweSample(u2);
+    delete[] bara2;
+    delete_TorusPolynomial(testvect2);
+    delete_LweSample(u1);
+    delete[] bara1;
+    delete_TorusPolynomial(testvect1);
+	delete_LweSample(temp_result2);
+	delete_LweSample(temp_result1);
+}
+
+
+
+
 
 
 #if 0
